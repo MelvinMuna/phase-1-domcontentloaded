c1059bdef21aef75a8dad8cf90b86083e1ce9385
Melvin Muna <melvinmuna@gmail.com>
2022-06-13 18:21:23 +0300

    commit tree 0c6190bb5b481c755177405aa9ec2d12cc12deb8

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    loadDOM()
+  });
+
+  function loadDOM(){
+      document.querySelector('#text')
+      .innerHTML = ("This is really cool!")
+  };
+ 
